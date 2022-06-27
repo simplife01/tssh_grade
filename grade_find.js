e1d6826584f1e029ef08880b17d52fa0154ad08c
simplife01 <jhuyuanchen@gmail.com>
2022-06-27 17:25:54 +0800

    20220627

---
+++ grade_find.js
@@ -0,0 +1,28 @@
+var customerForm = document.getElementById("customerForm");
+var stuNum = document.getElementById("stuNum");
+
+
+function afterSubmit(e){
+    e.preventDefault();
+
+    // var stu_num = {
+    //     number: stuNum.value,
+    // };
+    // console.log(JSON.stringify(stu_num));
+    const url = "https://script.google.com/macros/s/AKfycbz4hy1ft7u-AkHG9VpmqQk5_QwWQkXyvkM4zXkx54FpUqDpcaNmksP04edkvjBLKOwR/exec" + '?number=' + stuNum.value;
+    fetch(url)
+    .then(d => d.json())
+    .then(d => {
+        console.log(d);
+        document.getElementById("res").textContent = d.stuname;
+        document.getElementById("res1").textContent = d.semester1;
+        document.getElementById("res2").textContent = d.semester2;
+        document.getElementById("res3").textContent = d.semester3;
+        document.getElementById("res4").textContent = d.semester4;
+        document.getElementById("res5").textContent = d.semester5;
+        document.getElementById("res6").textContent = d.semester6;
+        customerForm.reset();
+    });
+}
+
+document.getElementById("submitButton").addEventListener("click", afterSubmit);
